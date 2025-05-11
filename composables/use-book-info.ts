593b77f02e8f1b338114716233bdfbff2c9d0f75
Ng Wing Tat, David <i@ngwingt.at>
2025-05-12 05:59:00 +0800

    ✨ Support EVM NFT Class for shelf & reader

--- composables/use-book-info.ts
+++ composables/use-book-info.ts
@@ -3,63 +3,62 @@ export default function ({ nftClassId = '' }: { nftClassId?: string } = {}) {
   const metadataStore = useMetadataStore()
   const bookstoreStore = useBookstoreStore()
 
+  const isEVM = computed(() => checkIsEVMAddress(nftClassId))
+
   const nftClass = computed(() => nftStore.getNFTClassById(nftClassId))
-  const iscnIdPrefix = computed(() => nftClass.value?.parent.iscn_id_prefix)
+  const legacyNFTClass = computed(() => nftStore.getLegacyNFTClassById(nftClassId))
+
+  const iscnIdPrefix = computed(() => legacyNFTClass.value?.parent.iscn_id_prefix)
   const iscnRecordData = computed(() => nftStore.getISCNDataByNFTClassId(nftClassId))
 
-  const publisherWalletAddress = computed(() => nftClass.value?.owner)
+  const publisherWalletAddress = computed(() => isEVM.value ? nftClass.value?.owner_address : legacyNFTClass.value?.owner)
   const publisherName = computed(() => {
     return metadataStore.getLikerInfoByWalletAddress(publisherWalletAddress.value)?.displayName
   })
 
-  const contentMetadata = computed(() => iscnRecordData.value?.contentMetadata)
+  const legacyContentMetadata = computed(() => iscnRecordData.value?.contentMetadata)
+  const contentMetadata = computed(() => nftClass.value?.metadata)
 
-  const name = computed(() => contentMetadata.value?.name)
-  const description = computed(() => contentMetadata.value?.description)
+  const name = computed(() => isEVM.value ? contentMetadata.value?.name : legacyContentMetadata.value?.name)
+  const description = computed(() => isEVM.value ? contentMetadata.value?.description : legacyContentMetadata.value?.description)
 
-  const author = computed(() => contentMetadata.value?.author)
+  const author = computed(() => isEVM.value ? contentMetadata.value?.author : legacyContentMetadata.value?.author)
   const authorName = computed(() => {
     return (typeof author.value === 'string' ? author.value : author.value?.name) || publisherName.value
   })
 
-  const coverSrc = computed(() => normalizeURIToHTTP(nftClass.value?.metadata.image))
+  const coverSrc = computed(() => normalizeURIToHTTP(isEVM.value ? contentMetadata.value?.thumbnailUrl : legacyNFTClass.value?.metadata.image))
 
   const publishedDate = computed(() => {
-    const datePublished = contentMetadata.value?.datePublished
+    const datePublished = isEVM.value ? contentMetadata.value?.datePublished : legacyContentMetadata.value?.datePublished
     return datePublished ? new Date(datePublished) : undefined
   })
 
   const releasedDate = computed(() => {
     if (publishedDate.value) return publishedDate.value
-    return new Date(nftClass.value?.created_at || Date.now())
+    return new Date((isEVM.value ? contentMetadata.value?.recordTimestamp : legacyNFTClass.value?.created_at) || Date.now())
   })
 
-  const externalURL = computed(() => contentMetadata.value?.url || contentMetadata.value?.external_url)
+  const externalURL = computed(() => isEVM.value ? contentMetadata.value?.external_url : legacyContentMetadata.value?.url || legacyContentMetadata.value?.external_url)
 
   const contentFingerprints = computed(() => {
-    return iscnRecordData.value?.contentFingerprints || []
+    return (isEVM.value ? contentMetadata.value?.contentFingerprints : iscnRecordData.value?.contentFingerprints) || []
   })
 
-  const nftClassContentURLs = computed(() => contentMetadata.value?.sameAs || [])
+  const nftClassContentURLs = computed(() => (isEVM.value ? contentMetadata.value?.sameAs : legacyContentMetadata.value?.sameAs) || [])
 
-  const nftClassReadActionTargets = computed(() => {
-    const { potentialAction } = contentMetadata.value || {}
-    if (!potentialAction) return []
-    let targets: ReadActionTarget[] = []
-    if (Array.isArray(potentialAction)) {
-      const readAction = potentialAction.find(
-        action => action.name === 'ReadAction',
-      )
-      if (!readAction) return [];
-      ({ targets } = readAction)
+  const nftClassReadActionEntryPoints = computed(() => {
+    const { potentialAction } = (isEVM.value ? contentMetadata.value : legacyContentMetadata.value) || {}
+    let readAction: ReadAction | undefined
+    if (potentialAction && potentialAction['@type'] === 'ReadAction') {
+      readAction = potentialAction as ReadAction
     }
-    else {
-      const readAction = potentialAction.ReadAction
-      if (!readAction) return [];
-      ({ targets } = readAction)
+    let entryPoints: ReadActionEntryPoint[] = []
+    if (readAction) {
+      entryPoints = readAction.target || []
     }
-    return targets.map((target) => {
-      const { contentType, url, name } = target
+    return entryPoints.map((entryPoint) => {
+      const { contentType, url, name } = entryPoint
       return {
         url: normalizeURIToHTTP(url),
         name,
@@ -69,8 +68,8 @@ export default function ({ nftClassId = '' }: { nftClassId?: string } = {}) {
   })
 
   const contentURLs = computed(() => {
-    if (nftClassReadActionTargets.value.length) {
-      return nftClassReadActionTargets.value
+    if (nftClassReadActionEntryPoints.value.length) {
+      return nftClassReadActionEntryPoints.value
     }
     return nftClassContentURLs.value.map(url => ({
       url: normalizeURIToHTTP(url),
@@ -93,8 +92,11 @@ export default function ({ nftClassId = '' }: { nftClassId?: string } = {}) {
   })
 
   return {
+    isEVM,
+
     iscnIdPrefix,
     nftClass,
+    legacyNFTClass,
 
     name,
     description,
