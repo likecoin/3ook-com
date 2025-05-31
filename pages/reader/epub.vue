e7e5d61ed7e667bcc0f183d3f2cc2236e77a937d
William Chong <me@williamchong.cloud>
2025-05-31 16:15:11 +0800

    ♻️ Refactor tts function

--- pages/reader/epub.vue
+++ pages/reader/epub.vue
@@ -243,6 +243,7 @@ import ePub, {
 } from 'epubjs'
 
 import type Section from 'epubjs/types/section'
+import { splitTextIntoSegments } from '~/utils/tts'
 
 declare interface EpubView {
   window: Window
@@ -478,34 +479,6 @@ async function loadEPub() {
     }
     percentage.value = book.locations.percentageFromCfi(location.start.cfi)
   })
-}
-
-function splitTextIntoSegments(text: string): string[] {
-  if (!text) return []
-  const punctuationRegex = /([.!?;。！？；：，、][\s\u200B]*)/
-  const segments = text.split(punctuationRegex)
-  const result: string[] = []
-
-  let currentSegment = ''
-  for (let i = 0; i < segments.length; i++) {
-    const segment = segments[i].trim()
-    if (!segment) continue
-    if (segment.length === 1 || currentSegment.length + segment.length < 100) {
-      currentSegment += segment
-    }
-    else {
-      if (currentSegment) {
-        result.push(currentSegment)
-      }
-      currentSegment = segment
-    }
-  }
-
-  if (currentSegment) {
-    result.push(currentSegment)
-  }
-
-  return result
 }
 
 function setActiveNavItemHref(href: string) {
