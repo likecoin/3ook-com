4a55ad71d75d2066e689653f2d285d850014aa8d
William Chong <me@williamchong.cloud>
2025-06-10 20:10:24 +0800

    ✨ Add post liker plus checkout and refresh API

--- stores/account.ts
+++ stores/account.ts
@@ -172,6 +172,11 @@ export const useAccountStore = defineStore('account', () => {
     await refreshSession()
   }
 
+  async function refreshSessionInfo() {
+    await $fetch('/api/account/refersh', { method: 'POST' })
+    await refreshSession()
+  }
+
   async function exportPrivateKey() {
     // NOTE: This function is only for login with Magic Link
     if (user.value?.loginMethod !== 'magic') {
@@ -212,6 +217,7 @@ export const useAccountStore = defineStore('account', () => {
     login,
     logout,
     updateSettings,
+    refreshSessionInfo,
     exportPrivateKey,
   }
 })
