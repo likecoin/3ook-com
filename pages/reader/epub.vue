efa15fbd3bd550dcc916c64f6c81ba97611cd887
AuroraHuang22 <aurora90376@gmail.com>
2025-06-23 23:26:36 +0800

    🐛 Filter navItems to include only sections in spine
    (cherry picked from commit 56aa96b1cadeee089d8e142c70424e403defc264)

--- pages/reader/epub.vue
+++ pages/reader/epub.vue
@@ -416,10 +416,12 @@ async function loadEPub() {
     if (!filename) return
     sectionHrefByFilename[filename] = section.href
   })
-  navItems.value = toc.map(item => ({
-    ...item,
-    href: sectionHrefByFilename[item.href] || item.href,
-  }))
+  navItems.value = toc
+    .map((item: NavItem) => {
+      const href = sectionHrefByFilename[item.href]
+      return href ? { ...item, href } : null
+    })
+    .filter((item: NavItem) => !!item)
   activeNavItemHref.value = book.spine.first().href
   lastSectionIndex.value = book.spine.last().index
 
