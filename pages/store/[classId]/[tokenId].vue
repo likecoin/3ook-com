b00f63394e6d19767bf8e486a173cd0278e994ff
William Chong <me@williamchong.cloud>
2025-06-25 18:43:18 +0800

    ✨ Add token-id page as redirect

---
+++ pages/store/[classId]/[tokenId].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const route = useRoute()
+const localeRoute = useLocaleRoute()
+const classId = computed(() => getRouteParam('classId'))
+await navigateTo(localeRoute({
+  name: 'store-classId',
+  params: { classId: classId.value },
+  query: route.query,
+}))
+</script>
