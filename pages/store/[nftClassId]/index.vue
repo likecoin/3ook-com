07f451b180b230d36b2ab2403c24860102cf46da
AuroraHuang22 <aurora90376@gmail.com>
2025-10-31 20:21:20 +0800

    💄 Display tags in Description tab

--- pages/store/[nftClassId]/index.vue
+++ pages/store/[nftClassId]/index.vue
@@ -65,6 +65,42 @@
                 v-html="bookInfoDescriptionHTML"
               />
             </ExpandableContent>
+            <ul
+              v-if="bookInfo.keywords.value"
+              :class="[
+                'flex',
+                'flex-wrap',
+                'gap-x-2',
+                'gap-y-4',
+                'mt-[48px]',
+                { 'max-tablet:hidden': isStakingTabActive },
+              ]"
+            >
+              <li
+                v-for="tag in bookInfo.keywords.value"
+                :key="tag"
+              >
+                <TagItem :label="tag" />
+              </li>
+            </ul>
+            <div class="flex flex-wrap gap-1 mt-3">
+              <TagItem
+                v-for="contentType in bookInfo.contentTypes.value"
+                :key="contentType"
+                :label="contentType.toUpperCase()"
+              />
+              <TagItem
+                :label="$t('reading_method_read_online')"
+              />
+              <TagItem
+                v-if="bookInfo.isDownloadable.value"
+                :label="$t('reading_method_download_file')"
+              />
+              <TagItem
+                v-if="!bookInfo.isAudioHidden.value && isLikerPlus"
+                :label="$t('product_page_support_tts_label')"
+              />
+            </div>
           </template>
 
           <template #author>
@@ -74,40 +110,6 @@
                 v-html="authorDescriptionHTML"
               />
             </ExpandableContent>
-          </template>
-
-          <template #file-info>
-            <table class="border-separate border-spacing-2">
-              <tbody
-                :class="[
-                  '[&>tr>td:first-child]:pr-4',
-                  'text-sm',
-                  '[&>tr>td:first-child]:text-gray-400',
-                  '[&>tr>td:last-child]:text-gray-900',
-                ]"
-              >
-                <tr>
-                  <td v-text="$t('product_page_book_format_label')" />
-                  <td v-text="$t('product_page_book_format_value')" />
-                </tr>
-                <tr v-if="bookInfo.formattedContentTypes">
-                  <td v-text="$t('product_page_file_format_label')" />
-                  <td v-text="bookInfo.formattedContentTypes" />
-                </tr>
-                <tr v-if="bookInfo.formattedReadingMethods">
-                  <td v-text="$t('product_page_reading_methods_label')" />
-                  <td v-text="bookInfo.formattedReadingMethods" />
-                </tr>
-                <tr>
-                  <td v-text="$t('product_page_support_tts_label')" />
-                  <td v-text="bookInfo.formattedTTSSupportLabel" />
-                </tr>
-                <tr v-if="bookInfo.formattedPublishedDate">
-                  <td v-text="$t('product_page_publish_date_label')" />
-                  <td v-text="bookInfo.formattedPublishedDate" />
-                </tr>
-              </tbody>
-            </table>
           </template>
 
           <template #staking-info>
@@ -177,25 +179,6 @@
             </div>
           </template>
         </UTabs>
-
-        <ul
-          v-if="bookInfo.keywords.value"
-          :class="[
-            'flex',
-            'flex-wrap',
-            'gap-x-2',
-            'gap-y-4',
-            'mt-[48px]',
-            { 'max-tablet:hidden': isStakingTabActive },
-          ]"
-        >
-          <li
-            v-for="tag in bookInfo.keywords.value"
-            :key="tag"
-          >
-            <TagItem :label="tag" />
-          </li>
-        </ul>
       </div>
 
       <div class="relative w-full tablet:max-w-[300px] laptop:max-w-[380px] shrink-0">
@@ -728,12 +711,6 @@ const infoTabItems = computed(() => {
       value: 'author',
     })
   }
-
-  items.push({
-    label: $t('product_page_info_tab_file_info'),
-    slot: 'file-info',
-    value: 'file-info',
-  })
 
   items.push({
     label: $t('staking_info_tab_staking_info'),
