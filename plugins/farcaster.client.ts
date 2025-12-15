d7520484612384a6ea6064a2dc4f62b3dc665131
William Chong <me@williamchong.cloud>
2025-12-15 22:29:40 +0800

    ⚡️ Only load miniapp-sdk if framed

--- plugins/farcaster.client.ts
+++ plugins/farcaster.client.ts
@@ -1,9 +1,10 @@
-import { sdk } from '@farcaster/miniapp-sdk'
-
 export default defineNuxtPlugin(async () => {
-  if (import.meta.client) {
+  // Don't load mini app SDK if not in a mini app environment
+  // https://github.com/farcasterxyz/miniapps/blob/9edfb783f40ce1b2504bfdf2e2f2e15815541771/packages/miniapp-sdk/src/sdk.ts#L35
+  if (import.meta.client && window && (!!window.ReactNativeWebView || window !== window.parent)) {
     try {
-      await sdk.actions.ready()
+      const { sdk: farcasterSdk } = await import('@farcaster/miniapp-sdk')
+      await farcasterSdk.actions.ready()
     }
     catch (error) {
       console.error('Failed to initialize Farcaster mini app SDK:', error)
