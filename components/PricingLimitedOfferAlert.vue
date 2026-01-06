williamchong/liker-land-v3

♻️ Refactor trial days config

components/PricingLimitedOfferAlert.vue
@@ -109,6 +109,7 @@
 
 <script lang="ts" setup>
 import brushCircle from '~/assets/images/brush-circle.png'
+import { DEFAULT_TRIAL_PERIOD_DAYS, DEFAULT_TRIAL_PRICE } from '~/constants/pricing'
 
 const { currency } = useSubscriptionPricing()
 
@@ -120,8 +121,8 @@ withDefaults(
   }>(),
   {
     isHidden: false,
-    trialPeriodDays: 7,
-    price: 1,
+    trialPeriodDays: DEFAULT_TRIAL_PERIOD_DAYS,
+    price: DEFAULT_TRIAL_PRICE,
   },
 )
 </script>

constants/pricing.ts
@@ -1,0 +1,3 @@
+export const DEFAULT_TRIAL_PERIOD_DAYS = 7
+export const PAID_TRIAL_PERIOD_DAYS = 30
+export const DEFAULT_TRIAL_PRICE = 1

likecoin/3ook-com

components/PricingLimitedOfferAlert.vue
@@ -109,6 +109,7 @@
 
 <script lang="ts" setup>
 import brushCircle from '~/assets/images/brush-circle.png'
+import { DEFAULT_TRIAL_PERIOD_DAYS, PAID_TRIAL_PRICE } from '~/constants/pricing'
 
 const { currency } = useSubscriptionPricing()
 
@@ -120,8 +121,8 @@ withDefaults(
   }>(),
   {
     isHidden: false,
-    trialPeriodDays: 7,
-    price: 1,
+    trialPeriodDays: DEFAULT_TRIAL_PERIOD_DAYS,
+    price: PAID_TRIAL_PRICE,
   },
 )
 </script>

constants/pricing.ts
@@ -207,3 +207,7 @@ export const TWD_PRICE_TIER_LIST = [
   2970,
   3000,
 ]
+
+export const DEFAULT_TRIAL_PERIOD_DAYS = 7
+export const PAID_TRIAL_PERIOD_DAYS_THRESHOLD = 30
+export const PAID_TRIAL_PRICE = 1