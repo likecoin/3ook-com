fe95c78aa50d2a577a20b3400d77162647487785
William Chong <me@williamchong.cloud>
2026-02-06 23:03:28 +0800

    🐛 Fix computed ref mutation in TTS player modal
    Set the underlying `startIndex` ref instead of mutating the read-only
    `ttsPlayerModalProps` computed directly in `openPlayer()`.

--- composables/use-tts-player-modal.ts
+++ composables/use-tts-player-modal.ts
@@ -53,7 +53,7 @@ export function useTTSPlayerModal(options: TTSPlayerOptions) {
     cfi?: string
   } & TTSPlayerModalProps = {}) {
     if (ttsIndex !== undefined) {
-      ttsPlayerModalProps.value.startIndex = ttsIndex
+      startIndex.value = ttsIndex
     }
     else if (sectionIndex !== undefined || cfi) {
       let segmentIndex = 0
@@ -73,10 +73,10 @@ export function useTTSPlayerModal(options: TTSPlayerOptions) {
           segmentIndex += cfiIndex - 1
         }
       }
-      ttsPlayerModalProps.value.startIndex = segmentIndex
+      startIndex.value = segmentIndex
     }
     else {
-      ttsPlayerModalProps.value.startIndex = 0
+      startIndex.value = 0
     }
     modal.open({ ...ttsPlayerModalProps.value, ...props })
   }
