745458367fae8d1a610ab54b805661afc5480341
Ng Wing Tat, David <i@ngwingt.at>
2026-05-20 10:54:04 +0800

    💄 Remove author from shelf item

--- app/components/BookshelfItem.vue
+++ app/components/BookshelfItem.vue
@@ -36,81 +36,67 @@
       </div>
     </div>
 
-    <div class="h-[70px]">
-      <div class="flex items-start gap-1">
-        <div
-          class="text-sm laptop:text-base text-highlighted font-semibold line-clamp-2 grow"
-          v-text="bookInfo.name"
-        />
-        <UDropdownMenu
-          v-if="isDesktopScreen"
-          :items="menuItems"
-          :modal="true"
-        >
-          <UButton
-            class="-mr-2 -mt-1"
-            icon="i-material-symbols-more-vert"
-            color="neutral"
-            variant="link"
-          />
-        </UDropdownMenu>
-        <UDrawer
-          v-else
-          v-model:open="isMobileMenuOpen"
-          :handle="false"
-        >
-          <UButton
-            class="-mr-2 -mt-1"
-            icon="i-material-symbols-more-vert"
-            color="neutral"
-            variant="link"
-          />
-          <template #content>
-            <UCard
-              class="pb-safe"
-              :ui="{ header: 'text-center font-bold' }"
-            >
-              <template #header>
-                {{ bookInfo.name.value }}
-              </template>
-              <UButton
-                v-for="item in menuItems"
-                :key="item.label"
-                class="cursor-pointer"
-                :icon="item.icon"
-                :label="item.label"
-                :href="item.href"
-                :to="item.to"
-                :disabled="item.disabled"
-                variant="link"
-                color="neutral"
-                size="xl"
-                block
-                :ui="{ base: 'justify-start' }"
-                @click="(e) => {
-                  isMobileMenuOpen = false
-                  item.onSelect?.(e)
-                }"
-              />
-            </UCard>
-          </template>
-        </UDrawer>
-      </div>
+    <!-- Use em unit for adapting to system font size changes -->
+    <div class="flex items-start gap-1 h-[3em] text-sm laptop:text-base">
+      <div
+        class="text-highlighted font-semibold line-clamp-2 grow"
+        v-text="bookInfo.name"
+      />
 
-      <div class="h-4 laptop:h-5 mt-0.5 truncate leading-none">
-        <NuxtLink
-          :to="bookInfo.getAuthorPageRoute({
-            llMedium: 'author-link',
-            llSource: 'bookshelf-item',
-          })"
-          :class="[
-            'inline',
-            'text-xs laptop:text-sm',
-            'text-toned hover:text-theme-black',
-            'hover:underline',
-          ]"
-        >{{ bookInfo.authorName }}</NuxtLink>
-      </div>
+      <UDropdownMenu
+        v-if="isDesktopScreen"
+        :items="menuItems"
+        :modal="true"
+      >
+        <UButton
+          class="-mr-2 -mt-1"
+          icon="i-material-symbols-more-vert"
+          color="neutral"
+          variant="link"
+        />
+      </UDropdownMenu>
+
+      <UDrawer
+        v-else
+        v-model:open="isMobileMenuOpen"
+        :handle="false"
+      >
+        <UButton
+          class="-mr-2 -mt-1"
+          icon="i-material-symbols-more-vert"
+          color="neutral"
+          variant="link"
+        />
+        <template #content>
+          <UCard
+            class="pb-safe"
+            :ui="{ header: 'text-center font-bold' }"
+          >
+            <template #header>
+              {{ bookInfo.name.value }}
+            </template>
+            <UButton
+              v-for="item in menuItems"
+              :key="item.label"
+              class="cursor-pointer"
+              :icon="item.icon"
+              :label="item.label"
+              :href="item.href"
+              :to="item.to"
+              :disabled="item.disabled"
+              variant="link"
+              color="neutral"
+              size="xl"
+              block
+              :ui="{ base: 'justify-start' }"
+              @click="(e) => {
+                isMobileMenuOpen = false
+                item.onSelect?.(e)
+              }"
+            />
+          </UCard>
+        </template>
+      </UDrawer>
     </div>
   </li>
 </template>
